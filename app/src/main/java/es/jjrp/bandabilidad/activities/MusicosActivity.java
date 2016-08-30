2ac704899a9713fc10930544d949142208df40fe
ramirezperezjj <ramirezperezjj@gmail.com>
2016-08-30 22:14:40 +0200

    Borrar y crear músicos

--- app/src/main/java/es/jjrp/bandabilidad/activities/MusicosActivity.java
+++ app/src/main/java/es/jjrp/bandabilidad/activities/MusicosActivity.java
@@ -1,6 +1,9 @@
 package es.jjrp.bandabilidad.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,7 @@ import es.jjrp.bandabilidad.bean.Musico;
 import es.jjrp.bandabilidad.dao.PersonDbHelper;
 
 public class MusicosActivity extends AppCompatActivity {
+    private static final int NUEVO_MUSICO_REQUEST = 1;
     PersonDbHelper pHelper;
 
     @Override
@@ -25,7 +29,9 @@ public class MusicosActivity extends AppCompatActivity {
         bNuevo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pHelper.createRow("nombre", "apellidos");
+                Intent intent = new Intent(getBaseContext(), NuevoMusicoActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+                startActivityForResult(intent, NUEVO_MUSICO_REQUEST);
 
                 cargarMusicos();
             }
@@ -39,21 +45,56 @@ public class MusicosActivity extends AppCompatActivity {
             }
         });
 
-
         pHelper = new PersonDbHelper(this);
-
 
         cargarMusicos();
     }
 
-    private void borrarMusico() {
-        ListView musicos = (ListView) findViewById(R.id.listMusicos);
-        if (musicos.getCheckedItemPosition() >= 0) {
-            String sel = musicos.getItemAtPosition(musicos.getCheckedItemPosition()).toString();
-            String id = sel.split("_")[0];
-            pHelper.deleteRow(Long.parseLong(id));
-            cargarMusicos();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case NUEVO_MUSICO_REQUEST:
+                cargarMusicos();
+                break;
+            default:
+                break;
         }
+
+    }
+
+    private void borrarMusico() {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+// 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_borrar_message)
+                .setTitle(R.string.dialog_borrar_title);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                ListView musicos = (ListView) findViewById(R.id.listMusicos);
+                if (musicos.getCheckedItemPosition() >= 0) {
+                    String sel = musicos.getItemAtPosition(musicos.getCheckedItemPosition()).toString();
+                    String musicoId = sel.split("_")[0];
+                    pHelper.deleteRow(Long.parseLong(musicoId));
+                    cargarMusicos();
+                }
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+// Set other dialog properties
+
+
+// Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
 
     }
 
---
+++ app/src/main/java/es/jjrp/bandabilidad/activities/NuevoMusicoActivity.java
@@ -0,0 +1,45 @@
+package es.jjrp.bandabilidad.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import es.jjrp.bandabilidad.R;
+import es.jjrp.bandabilidad.dao.PersonDbHelper;
+
+public class NuevoMusicoActivity extends AppCompatActivity {
+    PersonDbHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_musico);
+        db = new PersonDbHelper(this);
+        Button b = (Button) findViewById(R.id.btNuevoGuardar);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tNombre = (TextView) findViewById(R.id.textNuevoNombre);
+                TextView tApellido = (TextView) findViewById(R.id.textNuevoApellido);
+                if (tNombre.getText() == null || tNombre.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Falta el nombre", Toast.LENGTH_SHORT).show();
+                } else if (tApellido.getText() == null || tApellido.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Falta el apellido", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        db.createRow(tNombre.getText().toString(), tApellido.getText().toString());
+//                    finishActivity(Activity.RESULT_OK);
+                        finish();
+                    } catch (Exception e) {
+                        Toast.makeText(getBaseContext(), "Error creando músico: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+}
